28e95f51f6905e21e6abf77ece18010235733acb
arbaz9234 <arbaz9234@gmail.com>
2026-06-07 23:02:57 +0530

    Added AlwaysMusicianCharm  mod

---
+++ Always Active Tools/AlwaysMusicianCharm/AlwaysMusicianCharm/AlwaysMusicianCharm.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using GlobalSettings;
+using HarmonyLib;
+
+[BepInPlugin("com.arbaz9234.alwaysmusiciancharm", "Always Musician Charm", "1.0.0")]
+[BepInProcess("Hollow Knight Silksong.exe")]
+public class AlwaysMusicianCharm : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        new Harmony("com.arbaz9234.alwaysmusiciancharm").PatchAll();
+    }
+
+    [HarmonyPatch(typeof(ToolItem), nameof(ToolItem.IsEquipped), MethodType.Getter)]
+    private static class IsEquipped_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix(ToolItem __instance, ref bool __result)
+        {
+            if (__instance == Gameplay.MusicianCharmTool)
+                __result = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(ToolItem), nameof(ToolItem.IsEquippedHud), MethodType.Getter)]
+    private static class IsEquippedHud_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix(ToolItem __instance, ref bool __result)
+        {
+            if (__instance == Gameplay.MusicianCharmTool)
+                __result = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(ToolItemManager), nameof(ToolItemManager.IsToolEquipped))]
+    private static class IsToolEquipped_Patch
+    {
+        [HarmonyPostfix]
+        private static void Postfix(ToolItem tool, ref bool __result)
+        {
+            if (tool == Gameplay.MusicianCharmTool)
+                __result = true;
+        }
+    }
+}
---
+++ Always Active Tools/AlwaysMusicianCharm/README.md
@@ -0,0 +1,19 @@
+# Always have Spider Strings
+
+Permanently equip Spider Strings: the musician charm in Silksong.
+
+## Overview
+
+This mod equips Spider Strings: the musician charm in Silksong. Sophisticated Weaver device. Resonates with music played to amplify and enhance a performance.
+
+## Features
+
+- Your tools permanently have SpiderStrings
+- Resonates with music played to amplify and enhance a performance.
+- Expands and strengthens the effects of the Needolin.
+- It increases the range of the Needolin by 25% and increases the time enemies spend singing by 1.25-2.5 seconds.
+
+## Nexus Mods
+
+Download the compiled release here:  
+https://www.nexusmods.com/hollowknightsilksong/mods/886
